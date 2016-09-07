b1ae367bdd27de72b94008be14c0cd31c1c22349
orugal <kyo20052@gmail.com>
2016-09-06 21:36:55 -0500

    nuevos cambios a la plantilla

--- opcMenu.php
+++ opcMenu.php
@@ -1,30 +1,42 @@
 <ul class="nav navbar-nav floatRight">
-    <li class=""><a href="home">SOMOS NIO</a></li>
+    <li class="dropdown visible-sm visible-xs">
+    	<a href="#" class="dropdown-toggle" data-toggle="dropdown" >SOMOS NIO <b class="caret"></b></a>
+    	<ul class="dropdown-menu multi-column columns-3">
+    		<div class="row">
+    			<div class="col-sm-4">
+		            <ul class="multi-column-dropdown">
+	    				<li style="background:#eee"><a href="#"><strong>QUIÉNES SOMOS</strong></a></li>
+	    				<li style="background:#eee"><a href="#"><strong>TRABAJA CON NOSOTROS</strong></a></li>
+    				</ul>
+    			</div>
+    		</div>
+    	</ul>
+    </li>
 <!-- Menú sólo movil-->
     <li class="dropdown visible-sm visible-xs">
-	            <a href="#" class="dropdown-toggle" data-toggle="dropdown">PROYECTOS <b class="caret"></b></a>
-	            <ul class="dropdown-menu multi-column columns-3">
-		            <div class="row">
-			            <div class="col-sm-4">
-				            <ul class="multi-column-dropdown">
-					            <li style="background:#eee"><a href="#"><strong>PROYECTOS NIO</strong></a></li>
-					            <li class="divider"></li>
-					            <li><a style="color:#505052" href="proyecto.php">CIUDADELA NIO NEIVA</a></li>
-					            <li><a style="color:#505052" href="proyecto.php">MAR ADENTRO NEIVA</a></li>
-					            <li><a style="color:#505052" href="proyecto.php">IBAGUÉ TORRE KEA</a></li>
-				            </ul>
-			            </div>
-			            <div class="col-sm-4">
-				            <ul class="multi-column-dropdown">
-					            <li style="background:#eee"><a href="#"><strong>PRÓXIMOS LANZAMIENTOS</strong></a></li>
-					            <li class="divider"></li>
-				        		<li><a style="color:#505052" href="nuevos.php">CARTAGENA</a></li>
-					            <li><a style="color:#505052" href="nuevos.php">FLANDES</a></li>
-				            </ul>
-			            </div>
-		            </div>
-	            </ul>
-	        </li>
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">PROYECTOS <b class="caret"></b></a>
+        <ul class="dropdown-menu multi-column columns-3">
+            <div class="row">
+	            <div class="col-sm-4">
+		            <ul class="multi-column-dropdown">
+			            <li style="background:#eee"><a href="#"><strong>PROYECTOS NIO</strong></a></li>
+			            <li class="divider"></li>
+			            <li><a style="color:#505052" href="proyecto.php">CIUDADELA NIO NEIVA</a></li>
+			            <li><a style="color:#505052" href="proyecto.php">MAR ADENTRO NEIVA</a></li>
+			            <li><a style="color:#505052" href="proyecto.php">IBAGUÉ TORRE KEA</a></li>
+		            </ul>
+	            </div>
+	            <div class="col-sm-4">
+		            <ul class="multi-column-dropdown">
+			            <li style="background:#eee"><a href="#"><strong>PRÓXIMOS LANZAMIENTOS</strong></a></li>
+			            <li class="divider"></li>
+		        		<li><a style="color:#505052" href="nuevos.php">CARTAGENA</a></li>
+			            <li><a style="color:#505052" href="nuevos.php">FLANDES</a></li>
+		            </ul>
+	            </div>
+            </div>
+        </ul>
+    </li>
 
 
 <!-- Fin menú sólo movil-->
@@ -32,7 +44,19 @@
 
 
 
-
+	<li class="dropdown visible-lg visible-md">
+         <a class="dropdown-toggle" data-toggle="dropdown"	>
+          	SOMOS NIO <b class="glyphicon glyphicon-triangle-bottom mini right"></b>
+          	<ul class="dropdown-menu">
+		        <li>
+		        	<a href="quienes-somos" style="color:#505052" tabindex="-1">QUIÉNES SOMOS</a>
+		        </li>
+		        <li>
+		        	<a href="trabaja-con-nosotros" style="color:#505052" tabindex="-1">TRABAJA CON NOSOTROS</a>
+		        </li>
+			</ul>
+        </a>
+    </li>
     <li class="dropdown visible-lg visible-md">
          <a class="dropdown-toggle" data-toggle="dropdown"	>
           	PROYECTOS <b class="glyphicon glyphicon-triangle-bottom mini right"></b>
