0c4edbb6e30eb2ae65a575a5879cb78ba6672003
Farez Prieto Oficina <kyo20052@gmail.com>
2016-09-23 18:10:52 -0500

    Cambios

--- html/interfaz/menu_principal.php
+++ html/interfaz/menu_principal.php
@@ -1,71 +1,11 @@
 <?php
 	global $core;
 	global $id;
-	$opciones_secundario = $core->listarMenuPrincipal();
-	$queryNoticias		 = $db->GetAll(sprintf("SELECT * FROM principal WHERE id_padre=10 AND eliminado=0 ORDER BY id ASC limit 1"));	
-	$contador	=	0;
-	/*echo"<li>";	
-						echo"<a title='".$datos['titulo']."' href='#home'>
-								Inicio
-							</a>";
-					 echo"</li>";
-	foreach($opciones_secundario as $datos)
-	{
-		$clase	=	($datos['id'] == $id)?'opcionesMenuSel':'';
-		if($datos['id'] == 13)
-		{
-			echo"<li>";	
-				echo"<a title='".$datos['titulo']."' href='#quienesSomos'>
-						".$datos['titulo']."
-					</a>";
-			echo"</li>";
-		}
-		elseif($datos['id'] == 1190)
-		{
-			echo"<li>";	
-				echo"<a title='".$datos['titulo']."' href='#menu'>
-						".$datos['titulo']."
-					</a>";
-			echo"</li>";
-		}
-		elseif($datos['id'] == 1195)
-		{
-			echo"<li>";	
-				echo"<a title='".$datos['titulo']."' href='#galeria'>
-						".$datos['titulo']."
-					</a>";
-			echo"</li>";
-		}
-		elseif($datos['id'] == 1199)
-		{
-			echo"<li>";	
-				echo"<a title='".$datos['titulo']."' href='#reservacion'>
-						".$datos['titulo']."
-					</a>";
-			echo"</li>";
-		}
-		elseif($datos['id'] == 1203)
-		{
-			echo"<li>";	
-				echo"<a title='".$datos['titulo']."' href='#contacto'>
-						".$datos['titulo']."
-					</a>";
-			echo"</li>";
-		}
-		else
-		{
-			echo"<li>";	
-				echo"<a title='".$datos['titulo']."' href='"._DOMINIO."index.php?id=".$datos['id']."'>
-						".$datos['titulo']."
-					</a>";
-			echo"</li>";
-		}	
-
-		$contador++;	
-	}*/
+	$menuPrincipal = $core->listarMenuPrincipal();
 ?>
 
 <ul class="nav navbar-nav floatRight">
+
     <li class="dropdown visible-sm visible-xs">
     	<a href="#" class="dropdown-toggle" data-toggle="dropdown" >SOMOS NIO <b class="caret"></b></a>
     	<ul class="dropdown-menu multi-column columns-3">
@@ -79,6 +19,7 @@
     		</div>
     	</ul>
     </li>
+
 <!-- Menú sólo movil-->
     <li class="dropdown visible-sm visible-xs">
         <a href="#" class="dropdown-toggle" data-toggle="dropdown">PROYECTOS <b class="caret"></b></a>
@@ -107,7 +48,6 @@
 
 
 <!-- Fin menú sólo movil-->
-
 
 
 
@@ -150,6 +90,7 @@
     <li><a href="noticias">NOTICIAS</a></li>
     <li><a href="preguntas-frecuentes">PREGUNTAS FRECUENTES</a></li>
     <li class="dropdown visible-lg visible-md">
+
          <a class="dropdown-toggle" data-toggle="dropdown"	>
           	SERVICIO AL CLIENTE <b class="glyphicon glyphicon-triangle-bottom mini right"></b>
           	<ul class="dropdown-menu">
@@ -170,4 +111,6 @@
     </li>
 
     <li><a href="contacto">CONTÁCTANOS</a></li>
+
+
  </ul>
